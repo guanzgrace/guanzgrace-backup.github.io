63f29c4edf6cf871834a782392ea1e8bbd3dc766
Grace Guan <16graceg@gmail.com>
2016-08-24 20:28:44 -0700

    8/24/16 commit

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -17,3 +17,13 @@ $(function() {
         event.preventDefault();
     });
 });
+
+
+$(document).on('click','.navbar-collapse.in',function(e) {
+    if( $(e.target).is('a') && $(e.target).attr('class') != 'dropdown-toggle' ) {
+        $(this).collapse('hide');
+    }
+    
+    
+    }
+});
