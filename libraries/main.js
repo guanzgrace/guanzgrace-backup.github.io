3d12ffba1c7d63ff8692fddd03468f1b13463887
Grace Guan <16graceg@gmail.com>
2016-12-21 19:26:59 -0800

    comments

--- libraries/main.js
+++ libraries/main.js
@@ -1,3 +1,4 @@
+// @author Grace Guan. @edited 12/21/16. made with love in CA.
 $(".about").click(function() {
 	$("#home-container").fadeOut('fast', function() {
         $("#about-container").fadeIn('slow');
